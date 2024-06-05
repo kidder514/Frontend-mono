e13be407a99709f1365a3babb317d6d15f09148c
Lu <kidder5142@gmail.com>
2024-06-05 14:57:24 +1000

    test husky

---
+++ template/.lintstagedrc.js
@@ -0,0 +1,3 @@
+module.exports = {
+	'*.{json,md,mdx,css,scss,sass,html,yml,yaml,js,jsx,ts,tsx}': ['npm run codecheck:fix'],
+};
--- template/app/component/shared/Banner/ActionBanner.tsx
+++ template/app/component/shared/Banner/ActionBanner.tsx
@@ -2,6 +2,6 @@ export const ActionBanner = () => {
 	return (
 		<div>
 			<div>this is a banner</div>
-		</div>
+
 	);
 };
